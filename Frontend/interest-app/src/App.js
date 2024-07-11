1ddaa0228b898743de514d1f2a3a1ff0c5f2c8dc
Mehmedx7 <117067006+Mehmedx7@users.noreply.github.com>
2024-07-11 22:57:46 +0530

    login, home, register added

---
+++ Frontend/interest-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import Chat from './components/Chat';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
